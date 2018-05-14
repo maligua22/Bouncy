1920fbf2c854ffc7b13afc70c928725cf370c33d
maligua22 <maligua22@users.noreply.github.com>
2018-05-14 22:51:18 +0200

    Initial commit

---
+++ app/src/main/java/com/example/malig/bouncy/AyudaActividad.java
@@ -0,0 +1,13 @@
+package com.example.malig.bouncy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AyudaActividad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ayuda_actividad);
+    }
+}
---
+++ app/src/main/java/com/example/malig/bouncy/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.malig.bouncy;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void ayuda (View vista){
+
+        Intent intencion=new Intent(this, AyudaActividad.class);
+
+        startActivity(intencion);
+    }
+}
